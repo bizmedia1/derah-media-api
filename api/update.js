506138db33268c032c606258cd6bfe5078dd0cd2
bizmedia1 <victorisrael539@gmail.com>
2026-07-14 02:16:52 +0100

    Update update.js

--- api/update.js
+++ api/update.js
@@ -1,1 +1,49 @@
+export default function handler(req,res){
 
+res.setHeader("Access-Control-Allow-Origin","*");
+
+res.setHeader("Access-Control-Allow-Methods","POST,OPTIONS");
+
+res.setHeader("Access-Control-Allow-Headers","Content-Type");
+
+if(req.method==="OPTIONS"){
+
+return res.status(200).end();
+
+}
+
+if(req.method!=="POST"){
+
+return res.status(405).json({
+
+success:false,
+
+message:"Method Not Allowed"
+
+});
+
+}
+
+const {password}=req.body;
+
+if(password!==process.env.DERAH_ADMIN_PASSWORD){
+
+return res.status(401).json({
+
+success:false,
+
+message:"Unauthorized"
+
+});
+
+}
+
+return res.status(200).json({
+
+success:true,
+
+message:"Admin Verified"
+
+});
+
+}
