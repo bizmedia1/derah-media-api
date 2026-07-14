fa9f1ae8431fe563476baecdfb9bbb8e5535453e
bizmedia1 <victorisrael539@gmail.com>
2026-07-14 01:29:18 +0100

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -1,1 +1,52 @@
-hh
+import fs from "fs";
+import path from "path";
+
+export default function handler(req,res){
+
+const {platform,country}=req.query;
+
+if(!platform||!country){
+
+return res.status(400).json({
+
+success:false,
+
+message:"Platform and country are required."
+
+});
+
+}
+
+const filePath=path.join(process.cwd(),"data","payment-data.json");
+
+const raw=fs.readFileSync(filePath,"utf8");
+
+const database=JSON.parse(raw);
+
+const data=database?.[platform]?.[country];
+
+if(!data){
+
+return res.status(404).json({
+
+success:false,
+
+message:"Coming Soon"
+
+});
+
+}
+
+return res.status(200).json({
+
+success:true,
+
+platform,
+
+country,
+
+...data
+
+});
+
+}
