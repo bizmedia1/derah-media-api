97f16b6cff67dc5dd26fb86e61f9b8f14c3cf2fa
bizmedia1 <victorisrael539@gmail.com>
2026-07-14 01:48:07 +0100

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -2,7 +2,16 @@ import fs from "fs";
 import path from "path";
 
 export default function handler(req,res){
+res.setHeader("Access-Control-Allow-Origin","*");
 
+res.setHeader("Access-Control-Allow-Methods","GET,OPTIONS");
+
+res.setHeader("Access-Control-Allow-Headers","Content-Type");
+  if(req.method==="OPTIONS"){
+
+return res.status(200).end();
+
+  }
 const {platform,country}=req.query;
 
 if(!platform||!country){
